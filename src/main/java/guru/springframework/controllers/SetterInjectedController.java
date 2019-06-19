7a0f183299b766f752965c89537464dd96d99833
Yussef Soudan <yussefsoudan@gmail.com>
2019-06-19 20:39:09 +0100

    Initial and final commit

---
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -0,0 +1,20 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceImpl;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
